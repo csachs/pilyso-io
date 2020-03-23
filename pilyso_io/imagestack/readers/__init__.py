9dc9524edb0f3a5b856d50cea77e19acccd1e5c1
Christian Sachs <c.sachs@fz-juelich.de>
2020-03-23 13:16:17 +0100

    updated and re-enabled czi reader to work with current czifile

--- pilyso_io/imagestack/readers/__init__.py
+++ pilyso_io/imagestack/readers/__init__.py
@@ -1,7 +1,7 @@
 from .tiff import TiffImageStack
 from .ometiff import OMETiffImageStack
 
-# from .czi import CziImageStack
+from .czi import CziImageStack
 from .nd2 import ND2ImageStack
 from .ndip import NDIPImageStack
 
