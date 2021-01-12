3074bb307e5123a12b9d468ff4dee0d0090d1655
Christian Sachs <c.sachs@fz-juelich.de>
2021-01-12 16:11:52 +0100

    Fix of PyPI packaging mistake

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='pilyso_io',
-    version='0.0.1.dev3',
+    version='0.0.1.dev4',
     description='pilyso - image reading library',
     long_description='Python Image anaLYsis SOftware library',
     author='Christian Sachs',
     author_email='c.sachs@fz-juelich.de',
     url='https://github.com/modsim/pilyso-io',
     packages=find_packages(),
-    requires=['numpy', 'nd2file', 'tifffile', 'czifile'],
+    install_requires=['numpy', 'nd2file', 'tifffile', 'czifile'],
     extras_require={
         'ndip': ['opencv']
     },
